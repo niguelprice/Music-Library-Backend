5bc965f12e858926b609189edb67e044080648e6
Niguel Price <niguelprice@yahoo.com>
2022-11-28 13:34:04 -0500

    created new view function for getting songs by id + added new urls path for song detail view function, + in the music_library_backend folder

--- music_library/views.py
+++ music_library/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -20,6 +21,6 @@ def song_list(request):
 
 @api_view(['GET'])
 def song_detail(request, pk):
-
-    print(pk)
-    return Response(pk)
+    song = get_object_or_404(Song, pk=pk)
+    serializer = SongSerializer(song)
+    return Response(serializer.data)
